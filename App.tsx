8c0d127a6441503562bdff4de18ff8230f463767
Ulad Birukou <alorine99@gmail.com>
2023-09-21 22:04:12 +0300

    feat:add splashscreen & fix deps

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaView } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 function App(): JSX.Element {
+  useEffect(() => {
+    SplashScreen.hide();
+  });
   return <SafeAreaView />;
 }
 
